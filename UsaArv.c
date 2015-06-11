e59fb4dbb21aa2b27a94033cab9dc3c5fc2607f2
Paulo Júnior <pauloafpjunior@hotmail.com>
2015-06-10 22:06:49 -0300

    novos teste na main

--- UsaArv.c
+++ UsaArv.c
@@ -1,12 +1,31 @@
 #include "ArvBin.h"
 #include<stdio.h>
 
+void verificaSeExiste(ArvBin *arv, int item) {
+    if (contem(arv, item) == TRUE)
+        printf("O elemento %d foi encontrado!\n", item);
+    else
+        printf("O elemento %d nao foi encontrado!\n", item);
+}
+
 int main() {
-    ArvBin *minhaArv = criarNoFolha(2);
+    ArvBin *minhaArv = criarArvore();
 
     if (estahVazia(minhaArv))
         printf("Arvore vazia!\n");
 
+    minhaArv = inserir(minhaArv, 3);
+    minhaArv = inserir(minhaArv, 5);
+    minhaArv = inserir(minhaArv, 4);
+    minhaArv = inserir(minhaArv, 0);
+    minhaArv = inserir(minhaArv, 2);
+
+    verificaSeExiste(minhaArv, 3);
+    verificaSeExiste(minhaArv, 5);
+    verificaSeExiste(minhaArv, 4);
+    verificaSeExiste(minhaArv, 0);
+    verificaSeExiste(minhaArv, 2);
+    verificaSeExiste(minhaArv, -1);
 
 /*  minhaArv = inserir(minhaArv, 5);
     minhaArv = inserir(minhaArv, 3);
